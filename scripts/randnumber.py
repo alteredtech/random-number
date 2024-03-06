5e570904971686cf3ca3d368cac28efa30f13f5a
Michael Einreinhof <michael@einreinhof.com>
2024-03-05 23:09:54 -0700

    added missing dependency

--- scripts/randnumber.py
+++ scripts/randnumber.py
@@ -3,5 +3,6 @@
 import random
 import boto3
 
-print("Showing random number between 1-5")
-print(random.randrange(1,5))
+def main():
+    print("Showing random number between 1-5")
+    print(random.randrange(1,5))
--- scripts/randnumber2.py
+++ scripts/randnumber2.py
@@ -3,8 +3,9 @@
 import random
 import inquirer
 
-end_value = inquirer.text(message="Input end value for random number")
+def main():
+    end_value = inquirer.text(message="Input end value for random number")
 
-random_number = random.randint(1, int(end_value))
+    random_number = random.randint(1, int(end_value))
 
-print("Random number:", random_number)
+    print("Random number:", random_number)
