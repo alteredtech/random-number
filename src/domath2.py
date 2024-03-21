f321ecc282ae66917088052c64f10a1af6acfa64
Michael Einreinhof <michael@einreinhof.com>
2024-03-21 09:36:20 -0600

    Fixing class importing issues

--- src/domath2.py
+++ src/domath2.py
@@ -5,7 +5,7 @@
 # pylint: disable=import-error
 import random
 from InquirerPy import inquirer
-import classes.math as math  # noqa: E402
+from classes import math  # noqa: E402
 # pylint: enable=import-error
 
 
